37000126adc2780267efda706440065819de9300
James <jamesruntas@cmail.carleton.ca>
2021-01-15 15:18:38 -0500

    Initial commit

---
+++ 2.0 Lab Problem/2.1/src/Problem.java
@@ -0,0 +1,7 @@
+//James Runtas SYSC2004 Lab 0
+public class Problem {
+    public static void main(String[] args) {
+        System.out.println("James Runtas");
+        System.out.println("Student No. 101109175");
+    }
+}
